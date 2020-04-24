878b7ec1fdf289ee2582e0e35998b7a9286e4b1a
j.kunciene@gmail.com <48964713+jkunciene@users.noreply.github.com>
2020-04-24 18:08:46 +0300

    13skaidriu

--- js.js
+++ js.js
@@ -1,16 +1,34 @@
 
 let automobiliai=["VW", "Audi", "Jeep", "MINI", "Subaru", "Mazda", "Fiat", "Seat", "Honda", "Nissan"];
 
-console.log(automobiliai.length); //parodo masyvo ilgį
+//console.log(automobiliai.length); //parodo masyvo ilgį
 
 
 let lastElement= automobiliai.push("BMW");
-console.log(automobiliai);
+//console.log(automobiliai);
 
 let last= automobiliai.push("fiktyvus");
-console.log(automobiliai);
+//console.log(automobiliai);
 let trinuPaskutini= automobiliai.pop();
-console.log(automobiliai);
+//console.log(automobiliai);
+
+//istrinti is masyvo pradzios
+let pirmas= automobiliai.shift();
+//console.log(automobiliai);
+
+//splice metodas: 1-kur, 2-kiek, 3-ka ideti
+let naujasMasyvas=automobiliai.splice(4, 1, "splizinu");
+//console.log(automobiliai);
+
+//splice metodas: 1-nuo kurio pradeti 2-kiek istrinti
+automobiliai.splice(0, 5);
+//console.log(automobiliai);
+
+let kopijaAtsargai=automobiliai.slice();
+console.log(kopijaAtsargai);
+
+console.log(automobiliai.reverse());
+
 
 information = "<ul>";
 for (i = 0; i < automobiliai.length; i++) {
